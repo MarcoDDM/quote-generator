8dae055134b4f0a40508ab67b5c189521566f840
MarcoDDM <marantal2013@hotmail.com>
2021-02-01 01:27:05 -0300

    add loader function

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show loading
+
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide loading
+
+const complete = () => {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote from API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://immense-badlands-79686.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -18,26 +36,31 @@ async function getQuote() {
     } else {
         authorText.innerText = data.quoteAuthor;
     }
-
     //Reduce font size for long quotes
     if (data.quoteText.length > 120) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote')
     }
-    
-        
         quoteText.innerText = data.quoteText;
+        //Stop Loader, show quote
+        complete();
     } catch (error) {
         getQuote();
     }
 }
 
+//Twitter quote
 const tweetQuote = () => {
     const quote = quoteText.innerText;
     const author = authorText.innerText;
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank');
 } 
+
+//Event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuote();
