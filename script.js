ba4a14d1cb36c3ca800ec3740a36b12568baaf71
MarcoDDM <marantal2013@hotmail.com>
2021-01-31 22:07:48 -0300

    change on font size for long quotes

--- script.js
+++ script.js
@@ -11,14 +11,33 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        authorText.innerText = data.quoteAuthor;
-        quoteText.innerText = data.quoteText;
 
+    //If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === '') {
+        authorText.innerText = 'Unknown';
+    } else {
+        authorText.innerText = data.quoteAuthor;
+    }
+
+    //Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    
+        
+        quoteText.innerText = data.quoteText;
     } catch (error) {
         getQuote();
-
     }
 }
+
+const tweetQuote = () => {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+} 
 
 // On load
 getQuote();
